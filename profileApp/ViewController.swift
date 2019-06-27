f973ddde164f64205c69bc3b8a046c8f9eaad762
kor216465 <kor216465@gmail.com>
2019-06-27 23:27:36 +0900

    Initial Commit

---
+++ profileApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  profileApp
+//
+//  Created by Jeff Jeong on 2019. 6. 27..
+//  Copyright © 2019년 Tuentuenna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
